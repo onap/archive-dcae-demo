10544f9cc3a7bbef787003c7e89735d11d38aeaf
Carsten Lund <lund@research.att.com>
2017-02-18 18:23:18 +0000

    Changes for docker push in LF
    Change-Id: I2e4a2e0ae5afcc1665580b90072114f5598becde
    Signed-off-by: Carsten Lund <lund@research.att.com>

--- dcae-demo-controller/src/main/docker-build/push.sh
+++ dcae-demo-controller/src/main/docker-build/push.sh
@@ -8,12 +8,9 @@ cd $(dirname $(readlink -e $0))
 
 GITROOT=../../../../..
 VERSION=$(grep version $GITROOT/pom.xml | head -1 | sed 'sX</.*XX' | sed 's/.*>//')
-USER=$1
-PASSWORD=$2
-DOCKER_REG=$3
+DOCKER_REG=$1
 TAG=$DOCKER_REG/dcae-controller:$VERSION
 
 docker tag dcae-controller:$VERSION $TAG
-docker login -u $USER -p $PASSWORD $DOCKER_REG
 docker push $TAG
 
