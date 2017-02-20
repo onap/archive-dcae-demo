8e9a1b32e3f415054bd0d29c832e79e680398c02
Carsten Lund <lund@research.att.com>
2017-02-20 01:04:22 +0000

    Fixed bug in finding version to push.
    Change-Id: Iedf1fd2c29dc5605f1b8a2edbbc362df46e97eae
    Signed-off-by: Carsten Lund <lund@research.att.com>

--- dcae-demo-controller/src/main/docker-build/push.sh
+++ dcae-demo-controller/src/main/docker-build/push.sh
@@ -6,7 +6,7 @@ set -x
 ## ensure we are in the right directory.
 cd $(dirname $(readlink -e $0))
 
-GITROOT=../../../../..
+GITROOT=../../..
 VERSION=$(grep version $GITROOT/pom.xml | head -1 | sed 'sX</.*XX' | sed 's/.*>//')
 DOCKER_REG=$1
 TAG=$DOCKER_REG/dcae-controller:$VERSION
