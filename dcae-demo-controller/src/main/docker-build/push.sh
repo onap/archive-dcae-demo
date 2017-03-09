e7bfecbd3840ea7e68993ea89f2ab6d1a65659f0
Carsten Lund <lund@research.att.com>
2017-03-09 20:39:51 +0000

    [DCAE-1] Added the work STAGING to docker version
    Change-Id: I0b776418693699d55cf260cec98ea19872380c7e
    Signed-off-by: Carsten Lund <lund@research.att.com>

--- dcae-demo-controller/src/main/docker-build/push.sh
+++ dcae-demo-controller/src/main/docker-build/push.sh
@@ -14,15 +14,17 @@ TAG=$DOCKER_REG/openecomp/dcae-controller:$VERSION
 docker tag dcae-controller:$VERSION $TAG
 docker push $TAG
 
-TAG1=$DOCKER_REG/openecomp/dcae-controller:$VERSION-$(date +%Y%m%dT%H%M%S)
-docker tag dcae-controller:$VERSION $TAG1
-docker push $TAG1
 
 case $VERSION in
   *SNAPSHOT)
-    echo > /dev/null
+    TAG1=$DOCKER_REG/openecomp/dcae-controller:$VERSION-$(date +%Y%m%dT%H%M%S)
+    docker tag dcae-controller:$VERSION $TAG1
+    docker push $TAG1
     ;;
   *)
+    TAG1=$DOCKER_REG/openecomp/dcae-controller:$VERSION-STAGING-$(date +%Y%m%dT%H%M%S)
+    docker tag dcae-controller:$VERSION $TAG1
+    docker push $TAG1
     TAG2=$DOCKER_REG/openecomp/dcae-controller:latest
     docker tag dcae-controller:$VERSION $TAG2
     docker push $TAG2
