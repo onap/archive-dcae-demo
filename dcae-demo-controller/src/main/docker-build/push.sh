97ae63ec66b8526e1a40168f975bc616b89a76a2
Carsten Lund <lund@research.att.com>
2017-03-23 14:55:54 +0000

    [DCAE-1] Added addtional tag to docker release job
    Change-Id: Iac96a45a63a9e00ea98ce59a1647d51bd7a1b683
    Signed-off-by: Carsten Lund <lund@research.att.com>

--- dcae-demo-controller/src/main/docker-build/push.sh
+++ dcae-demo-controller/src/main/docker-build/push.sh
@@ -8,6 +8,7 @@ cd $(dirname $(readlink -e $0))
 
 GITROOT=../../..
 VERSION=$(grep version $GITROOT/pom.xml | head -1 | sed 'sX</.*XX' | sed 's/.*>//')
+VERSION2=$(echo $VERSION | cut -d . -f1,2)
 DOCKER_REG=$1
 TAG=$DOCKER_REG/openecomp/dcae-controller:$VERSION
 
@@ -28,6 +29,9 @@ case $VERSION in
     TAG2=$DOCKER_REG/openecomp/dcae-controller:latest
     docker tag dcae-controller:$VERSION $TAG2
     docker push $TAG2
+    TAG3=$DOCKER_REG/openecomp/dcae-controller:$VERSION2-STAGING-latest
+    docker tag dcae-controller:$VERSION $TAG3
+    docker push $TAG3
     ;;
 esac
 
